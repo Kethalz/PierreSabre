abfabcf6c27ab4dd803669311d7bb661f8df2523
Romain Luque <lqr3792a@ensfsi.onmicrosoft.com>
2025-11-25 20:02:54 +0100

    Presque fini méthode provoquer gagner perdre

--- src/personnage/Ronin.java
+++ src/personnage/Ronin.java
@@ -13,4 +13,18 @@ public class Ronin extends Humain {
 		this.argent-= don;
 		parler( beneficiaire.getNom() + " prend ces " + don + " sous.");
 	}
+	
+	public void provoquer(Yakuza adversaire) {
+		int force = 2*this.honneur;
+		if (force>=adversaire.getStreetCred()) {
+			this.argent+=adversaire.getArgent();
+			adversaire.perdre();
+			parler("Je t'ai eu petit Yakuza");
+		}else {
+			this.honneur--;
+			this.argent=0;
+			parler("J'ai perdu contre ce Yakuza, mon honneur et ma bourse ont en pris un coup");
+			adversaire.gagner(this.argent);
+		}
+	}
 }
--- src/personnage/Yakuza.java
+++ src/personnage/Yakuza.java
@@ -5,6 +5,10 @@ public class Yakuza extends Humain{
 	private String clan;
 	private int streetcred = 0;
 	
+	public int getStreetCred() {
+		return streetcred;
+	}
+
 	public Yakuza(String nom, String boissonFavorite, int argent, String clan) {
 		super(nom, boissonFavorite, argent);
 		this.clan = clan;
@@ -20,5 +24,16 @@ public class Yakuza extends Humain{
 		parler("J'ai piqué les " + extortion + "sous de " + victime.getNom() +", ce qui me fait 45 sous dans ma poche. Hi ! Hi !");
 	}
 	
+	public int perdre() {
+		int perte = this.argent;
+		this.argent = 0;
+		parler("J'ai perdu mon duel et mes " + perte + "sous, snif ... J'ai déshonoré le clan de " + this.clan);
+		return perte;
+	}
 	
+	public void gagner(int gain) {
+		this.argent+=gain;
+		parler("Ce ronin pensait vraiment battre " + getNom() + "du clan " + this.clan
+		+  "? Je l'ai dépouillé de ses " + gain + "sous");
+	}
 }
